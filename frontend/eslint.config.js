88cd51057293effe791d13e606ce296ddd928fad
Roko <roko.kokeza@gostudion.com>
2025-03-16 10:53:45 +0100

    chore: eslint import

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,12 +1,30 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: [
+            "../**", // Prevent relative parent imports
+            "src/components/atoms/*", // Enforce alias @atoms
+            "src/components/molecules/*", // Enforce alias @molecules
+            "src/components/organisms/*", // Enforce alias @organisms
+          ],
+        },
+      ],
+    },
+  },
 ];
+
--- frontend/src/components/organism/addCommentElement/AddCommentElement.tsx
+++ frontend/src/components/organism/addCommentElement/AddCommentElement.tsx
@@ -1,9 +1,9 @@
-import Image from "../../atoms/image/Image";
-import TextArea from "../../atoms/textarea/TextArea";
+import Image from "@atoms/image/Image";
+import TextArea from "@atoms/textarea/TextArea";
 
 import "./addCommentElement.css";
 import { useState } from "react";
-import Button from "../../atoms/button/Button";
+import Button from "@atoms/button/Button";
 
 type Props = {
   avatar: string;
