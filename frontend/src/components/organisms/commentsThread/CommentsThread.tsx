f6760b9699f87a85a38f7c5484f74197f15c7034
Roko <roko.kokeza@gostudion.com>
2025-03-16 14:09:26 +0100

    chore: folder structure

--- frontend/src/components/organisms/commentsThread/CommentsThread.tsx
+++ frontend/src/components/organisms/commentsThread/CommentsThread.tsx
@@ -4,8 +4,8 @@ import { useCallback } from "react";
 
 import { useComments } from "@hooks/useComments";
 import AddCommentElement from "@molecules/addCommentElement/AddCommentElement";
-import { Modal } from "@molecules/modal/Modal";
 import CommentCard from "@organisms/commentCard/CommentCard";
+import { Modal } from "@organisms/modal/Modal";
 import {
   requestObjects,
   requestUrls,
